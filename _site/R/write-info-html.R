b5dba118a4ff20b34d3c6469fb1350ffd41f16c1
ercbk <ericbookax0@yahoo.com>
2021-08-01 22:07:03 -0400

    gallery: changed text in viewer from Reference to Project

--- _site/R/write-info-html.R
+++ _site/R/write-info-html.R
@@ -3,7 +3,7 @@ write_info_html <- function (code, ref) {
   info_html <- tagList(
     p('Code: ',
       a(href = code, 'script')),
-    p('Reference: ',
+    p('Project: ',
       a(href = ref, 'link'))
   )
   html_string <- stringr::str_remove_all(toString(info_html), "\n")
