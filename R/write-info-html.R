557919efb185ace8932e3ca7d6d947e797a3e155
ercbk <ericbookax0@yahoo.com>
2021-08-02 15:50:25 -0400

    gallery: minor edit

--- R/write-info-html.R
+++ R/write-info-html.R
@@ -1,10 +1,10 @@
 write_info_html <- function (code, ref) {
   
   info_html <- tagList(
-    p('Code: ',
-      a(href = code, 'script')),
     p('Project: ',
-      a(href = ref, 'link'))
+      a(href = ref, 'link')),
+    p('Code: ',
+      a(href = code, 'script'))
   )
   html_string <- stringr::str_remove_all(toString(info_html), "\n")
 }
