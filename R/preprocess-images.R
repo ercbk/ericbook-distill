4c162e2d2d64087b8a49c3af22841433b6d89e79
ercbk <ericbookax0@yahoo.com>
2021-08-01 20:32:06 -0400

    gallery: thumbnail order and sizes finished

--- R/preprocess-images.R
+++ R/preprocess-images.R
@@ -1,14 +1,19 @@
 # Processing images
 
-library(dplyr)
+
 
 source("R/create-thumbnail.R")
 
 png_paths <- fs::dir_ls("images/gallery/large")
 
-purrr::walk(png_paths, function(x) {
-  image <- basename(x)
-  create_thumbnail(image)
-})
-  
+images <- purrr::map_chr(png_paths, ~basename(.x))
+
+create_thumbnail(images[[1]], "28%")
+create_thumbnail(images[[2]], "18%")
+create_thumbnail(images[[3]], "5%")
+create_thumbnail(images[[4]], "5%")
+create_thumbnail(images[[5]], "18%")
+create_thumbnail(images[[6]], "13%")
+create_thumbnail(images[[7]], "6%")
+
 
