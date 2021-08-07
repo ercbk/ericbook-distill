6a7dfe99d9a6b59a51cec1fe7a26f1cc3ec1562f
ercbk <ericbookax0@yahoo.com>
2021-08-06 21:15:08 -0400

    gallery: changed median-age img res

--- R/preprocess-images.R
+++ R/preprocess-images.R
@@ -21,7 +21,7 @@ create_thumbnail(images[[2]], "18%")
 create_thumbnail(images[[3]], "5%")
 create_thumbnail(images[[4]], "5%")
 create_thumbnail(images[[5]], "18%")
-create_thumbnail(images[[6]], "13%")
+create_thumbnail(images[[6]], "18%")
 create_thumbnail(images[[7]], "6%")
 
 
